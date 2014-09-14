5839dbc7e98a9917e51faa8fa2fc30709a9ddd92
Matias Anaya <matiasanaya@gmail.com>
2014-09-14 16:25:29 +1000

    Rewrite specs for LineItem::Parser
    It now expects strings for :unit_price. In addition, the code
    was simplified, using the #include expectation instead of #eql.

--- spec/line_item_parser_spec.rb
+++ spec/line_item_parser_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe SalesTax::LineItem::Parser do
 
   shared_examples 'a correct parser' do |_in, _out|
     it 'does the job' do
-      expect(parser.parse(_in)).to eql _out
+      expect(parser.parse(_in)).to include _out
     end
   end
 
@@ -17,65 +17,53 @@ RSpec.describe SalesTax::LineItem::Parser do
 
   context 'when invalid input' do
     context 'with missing arguments' do
-      it_behaves_like 'a complaining parser', '1, description'
+      it_behaves_like 'a complaining parser',
+                      '1, description'
     end
 
     context 'with too many arguments' do
-      it_behaves_like 'a complaining parser', '1, description, 12.49, 1'
+      it_behaves_like 'a complaining parser',
+                      '1, description, 12.49, 1'
     end
 
     context 'with arguments in wrong order' do
-      it_behaves_like 'a complaining parser', '12.49, description, 1'
+      it_behaves_like 'a complaining parser',
+                      '12.49, description, 1'
     end
 
     context 'with no description' do
-      it_behaves_like 'a complaining parser', '1, , 12.49'
+      it_behaves_like 'a complaining parser',
+                      '1, , 12.49'
     end
   end
 
   context 'when valid input' do
     context 'control' do
-      line_item = '1, description, 12.49'
-      parsed_line_item = {
-        quantity: 1,
-        description: ' description',
-        unit_price: BigDecimal('12.49')
-      }
-
-      it_behaves_like 'a correct parser', line_item, parsed_line_item
+      it_behaves_like 'a correct parser',
+                      '1, description, 12.49',
+                      {
+                        quantity: 1,
+                        description: ' description',
+                        unit_price: '12.49'
+                      }
     end
 
     context 'with quantity' do
-      line_item = '5, description, 0.0'
-      parsed_line_item = {
-        quantity: 5,
-        description: ' description',
-        unit_price: BigDecimal('0.0')
-      }
-
-      it_behaves_like 'a correct parser', line_item, parsed_line_item
+      it_behaves_like 'a correct parser',
+                      '5, description, 0.0',
+                      {quantity: 5}
     end
 
     context 'with description' do
-      line_item = '0, a imported description, 0.0'
-      parsed_line_item = {
-        quantity: 0,
-        description: ' a imported description',
-        unit_price: BigDecimal('0.0')
-      }
-
-      it_behaves_like 'a correct parser', line_item, parsed_line_item
+      it_behaves_like 'a correct parser',
+                      '0, a imported description, 0.0',
+                      {description: ' a imported description'}
     end
 
     context 'with unit price' do
-      line_item = '0, description, 12.49'
-      parsed_line_item = {
-        quantity: 0,
-        description: ' description',
-        unit_price: BigDecimal('12.49')
-      }
-
-      it_behaves_like 'a correct parser', line_item, parsed_line_item
+      it_behaves_like 'a correct parser',
+                      '0, description, 12.49',
+                      {unit_price: '12.49'}
     end
   end
 end
