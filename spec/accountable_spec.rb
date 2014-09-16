81d6baafca09e5480d9e5e738911fe7ebb129821
Matias Anaya <matiasanaya@gmail.com>
2014-09-16 12:06:42 +1000

    Update Accountable module specs

--- spec/accountable_spec.rb
+++ spec/accountable_spec.rb
@@ -5,53 +5,59 @@ require_relative 'shared_examples_for_taxable'
 RSpec.describe SalesTax::Accountable do
   AccountableDouble = Struct.new(:unit_price_str, :imported?, :category) do
     include SalesTax::Accountable
-    def to_hash
-      accountable_to_hash.merge({
-        unit_price: unit_price_str
-      })
-    end
   end
-  let(:accountable_double) {
-    AccountableDouble.new(BigDecimal('11.25'), imported?, category)
+
+  let(:accountable) {
+    AccountableDouble.new('11.25', true, :other)
   }
 
   describe 'AccountableDouble' do
-    let(:object) { AccountableDouble.new(BigDecimal('11.25'), true, :other) }
+    let(:object) { accountable }
     it_behaves_like 'a accountable'
     it_behaves_like 'a taxable'
   end
-  describe '#unit_sales_tax' do
-    subject { accountable_double.unit_sales_tax }
 
-    context 'when basic exempt' do
-      ['food', 'medicine', 'book'].each do |cat|
-        context "when #{cat}" do
-          let(:category) { cat.to_sym }
+  describe '#to_hash' do
+    describe '[:total_unit_price]' do
+      subject { accountable.to_hash[:unit_sales_tax] }
+      it { is_expected.to eq '1.7'}
+    end
 
-          context 'when local' do
-            let(:imported?) { false }
-            it { is_expected.to eq BigDecimal('0') }
-          end
+    describe '[:unit_sales_tax]'do
+      subject {
+        AccountableDouble.new('11.25', imported?, category).to_hash[:unit_sales_tax]
+      }
 
-          context 'when imported' do
-            let(:imported?) { true }
-            it { is_expected.to eq BigDecimal('0.6') }
+      context 'when basic exempt' do
+        ['food', 'medicine', 'book'].each do |cat|
+          context "when #{cat}" do
+            let(:category) { cat.to_sym }
+
+            context 'when local' do
+              let(:imported?) { false }
+              it { is_expected.to eq '0.0' }
+            end
+
+            context 'when imported' do
+              let(:imported?) { true }
+              it { is_expected.to eq '0.6' }
+            end
           end
         end
       end
-    end
 
-    context 'when basic applicable' do
-      let(:category) { :other }
+      context 'when basic applicable' do
+        let(:category) { :other }
 
-      context 'when local' do
-        let(:imported?) { false }
-        it { is_expected.to eq BigDecimal('1.15') }
-      end
+        context 'when local' do
+          let(:imported?) { false }
+          it { is_expected.to eq '1.15' }
+        end
 
-      context 'when imported' do
-        let(:imported?) { true }
-        it { is_expected.to eq BigDecimal('1.7') }
+        context 'when imported' do
+          let(:imported?) { true }
+          it { is_expected.to eq '1.7' }
+        end
       end
     end
   end
