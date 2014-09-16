d0a3e220fde8dfc36a3b3ac2907bacfc1408d742
Matias Anaya <matiasanaya@gmail.com>
2014-09-16 12:03:44 +1000

    Rewrite Accountable and Taxable roles specs

--- spec/shared_examples_for_accountable.rb
+++ spec/shared_examples_for_accountable.rb
@@ -1,11 +1,9 @@
 RSpec.shared_examples 'a accountable' do
-  subject { object }
-  describe 'the public interface' do
-    it { is_expected.to respond_to :unit_sales_tax, :to_hash }
-  end
-
   describe '#to_hash' do
     subject { object.to_hash }
+
+    it { is_expected.to be_instance_of Hash }
+
     it { is_expected.to include :unit_sales_tax, :total_unit_price }
   end
 end
--- spec/shared_examples_for_taxable.rb
+++ spec/shared_examples_for_taxable.rb
@@ -1,21 +1,16 @@
 RSpec.shared_examples 'a taxable' do
-  describe 'the public interface' do
-    subject { object }
-    it { is_expected.to respond_to :unit_price_str, :imported?, :category }
-  end
-
   describe '#unit_price_str' do
-    subject { object.unit_price_str }
-    it { is_expected.to be_instance_of BigDecimal }
+    subject { object.send(:unit_price_str) }
+    it { is_expected.to be_instance_of String }
   end
 
   describe '#imported?' do
-    subject { object.imported? }
-    it { is_expected.to_not be_nil }
+    subject { object.send(:imported?) }
+    it { is_expected.to satisfy { |v| v.instance_of?(TrueClass) || v.instance_of?(FalseClass) } }
   end
 
   describe '#category' do
-    subject { object.category }
-    it { is_expected.to_not be_nil }
+    subject { object.send(:category) }
+    it { is_expected.to be_instance_of Symbol }
   end
 end
