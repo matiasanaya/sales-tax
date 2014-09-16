56f1975419428f3bce7450fac9c32864ad469317
Matias Anaya <matiasanaya@gmail.com>
2014-09-16 12:31:24 +1000

    Rewrite Receipt#print for readability

--- lib/sales_tax/receipt.rb
+++ lib/sales_tax/receipt.rb
@@ -9,19 +9,10 @@ module SalesTax
     end
 
     def print
-      total = BigDecimal('0')
-      sales_taxes_total = BigDecimal('0')
-
-      _print = ""
-      items.each do |item|
-        total += BigDecimal(item[:total_unit_price])
-        sales_taxes_total += BigDecimal(item[:unit_sales_tax])
-        _print << print_item(item)
-      end
+      _print = ''
+      _print << print_items
       _print << "\n"
-      _print << print_sales_taxes_total(sales_taxes_total)
-      _print << print_total(total)
-
+      _print << print_totals
       _print
     end
 
@@ -29,9 +20,33 @@ module SalesTax
 
     attr_accessor :items
 
-    def print_total(bd)
-      total = format_to_money_string(bd.to_s('F'))
-      "Total: #{total}\n"
+    def print_items
+      _items = ''
+      items.each { |i| _items << print_item(i) }
+      _items
+    end
+
+    def print_item(item)
+      taxed_price_str = format_to_money_string(item[:total_unit_price])
+      "#{item[:quantity]}, #{item[:description]}, #{taxed_price_str}\n"
+    end
+
+    def print_totals
+      total, sales_taxes_total = calculate_totals
+      _totals = ''
+      _totals << print_sales_taxes_total(sales_taxes_total)
+      _totals << print_total(total)
+      _totals
+    end
+
+    def calculate_totals
+      total = BigDecimal('0')
+      sales_taxes_total = BigDecimal('0')
+      items.each do |item|
+        total += BigDecimal(item[:total_unit_price])
+        sales_taxes_total += BigDecimal(item[:unit_sales_tax])
+      end
+      [total, sales_taxes_total]
     end
 
     def print_sales_taxes_total(bd)
@@ -39,9 +54,9 @@ module SalesTax
       "Sales Taxes: #{st_total}\n"
     end
 
-    def print_item(item)
-      taxed_price_str = format_to_money_string(item[:total_unit_price])
-      "#{item[:quantity]}, #{item[:description]}, #{taxed_price_str}\n"
+    def print_total(bd)
+      total = format_to_money_string(bd.to_s('F'))
+      "Total: #{total}\n"
     end
 
     def format_to_money_string(str)
